c9acced73d760e542cd2e2f172c6b9a0162cb7cc
shunkun <y.y.22.t.h.1@gmail.com>
2021-09-22 23:49:29 +0900

    initialize

---
+++ MiniApp/MiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp
+//
+//  Created by hmhm on 2021/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
