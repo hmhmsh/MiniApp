53a3c8968af36da2b724fc5109bc7c1cba5cda66
shunkun <y.y.22.t.h.1@gmail.com>
2021-09-23 12:06:06 +0900

    add preview for Xcode 13

--- MiniApp/Package/Sources/Home/HomeViewController.swift
+++ MiniApp/Package/Sources/Home/HomeViewController.swift
@@ -5,11 +5,32 @@
 //  Created by hmhm on 2021/09/22.
 //
 
+import SwiftUI
 import UIKit
 
-public class HomeViewController: UIViewController {
+public final class HomeViewController: UIViewController {
     public static var instance: HomeViewController {
         UIStoryboard(name: "Home", bundle: .module).instantiateInitialViewController()!
     }
 }
 
+struct HomeViewControllerPreviews: PreviewProvider {
+    static var previews: some View {
+        return UIViewRepresented { _ in
+            HomeViewController.instance.view
+        }
+    }
+}
+
+struct UIViewRepresented<UIViewType>: UIViewRepresentable where UIViewType: UIView {
+    let makeUIView: (Context) -> UIViewType
+    let updateUIView: (UIViewType, Context) -> Void = { _, _ in }
+
+    func makeUIView(context: Context) -> UIViewType {
+        self.makeUIView(context)
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        self.updateUIView(uiView, context)
+    }
+}
